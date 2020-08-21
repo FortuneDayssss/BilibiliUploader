3d3c954b3d98f37dc514dc8ce8fe07119240ab9e
FortuneDayssss <717622995@qq.com>
2020-08-21 02:21:48 -0400

    release version 0.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='bilibiliuploader',
-    version="0.0.3",
+    version="0.0.4",
     packages=['bilibiliuploader', 'bilibiliuploader.util'],
     url='https://github.com/FortuneDayssss/BilibiliUploader',
     install_requires=['certifi>=2020.4.5.1',
